4e4bc09a7ff45cddda7c8ad8f0a1d452f3b021b5
Jakhongir Saydaliev <68238593+Jakhongir0103@users.noreply.github.com>
2022-09-10 10:44:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import pathlib
 import platform
 
 temp = pathlib.PosixPath
-pathlib.WindowsPath = pathlib.PureWindowsPath
-st.title(f'wp: {pathlib.PureWindowsPath}')
+st.title(f'pwp: {pathlib.PureWindowsPath}')
 st.title(f'wp: {pathlib.WindowsPath}')
 st.title(f'pp: {pathlib.PosixPath}')
+pathlib.WindowsPath = pathlib.PureWindowsPath
 
 # plt = platform.system()
 # if plt == 'Linux': 
