74c130c51e5b99bfab72f1bee35fc0e13a66856b
Jakhongir Saydaliev <68238593+Jakhongir0103@users.noreply.github.com>
2022-09-10 10:14:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ import platform
 plt = platform.system()
 if plt == 'Linux': 
     st.title(f'plt: {plt}')
-    st.title(f'wp: {pathlib.WindowsPath}')
-    st.title(f'pp: {pathlib.PosixPath}')
+    st.title(f'wp: {pathlib.WindowsPath()}')
+    st.title(f'pp: {pathlib.PosixPath()}')
     pathlib.WindowsPath = pathlib.PosixPath
-    st.title(f'wp2: {pathlib.WindowsPath}')
-    st.title(f'pp2: {pathlib.PosixPath}')
+    st.title(f'wp2: {pathlib.WindowsPath()}')
+    st.title(f'pp2: {pathlib.PosixPath()}')
 
 # title
 # st.title("Transport classification")
